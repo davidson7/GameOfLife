07dd13d5f08769e90d3f6bdf33b2d6b9e1092f4c
Dana <ddavidson@chapman.edu>
2019-02-23 21:45:54 +0000

    Added some files, everything works becuase they don't do anything

---
+++ ClassicBoundary.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Simulation.h"
+using namespace std;
+
+class ClassicBoundary{
+public:
+  ClassicBoundary();
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  cout<<"Would you like to use a map file(m) or random generation(r)?"<<endl;
+  cin
+}
