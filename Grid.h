ff63bbee2b52dc61e85f0d30a9ca90bf67c77781
Dana <ddavidson@chapman.edu>
2019-02-24 02:51:46 +0000

    added grid.h

---
+++ Grid.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Grid{
+public:
+  Grid();
+  Grid(int width, int length);
+  ~Grid();
+
+  void setDimensions(int width, int length);
+
+private:
+  int width;
+  int length;
+}
