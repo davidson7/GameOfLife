98f4066ee55e4bbdda59f25c1e1fbc3e276732e5
Dana <ddavidson@chapman.edu>
2019-03-01 20:19:05 +0000

    added constructors, redefining methods

---
+++ GeneralMode.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include 'GeneralMode.h'
+
+using namespace std;
+
+GeneralMode::GeneralMode(){
+
+}
+
+GeneralMode::~GeneralMode(){
+
+}
+
+int GeneralMode::getNeighbors(int x, int y){
+  
+}
